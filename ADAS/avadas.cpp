88821a4f2503191b5f6750c788199692fb4a5fc2
ceca5556 <ceca5556@colorado.edu>
2024-12-07 11:40:36 -0700

    changed opencv includes to just one overarching include

--- ADAS/avadas.cpp
+++ ADAS/avadas.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+// #include <opencv2/highgui/highgui.hpp>
 
-#include <opencv2/imgproc.hpp>
+// #include <opencv2/imgproc.hpp>
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/core/base.hpp>
-#include <opencv2/core/types.hpp>
+// #include <opencv2/core/core.hpp>
+// #include <opencv2/core/base.hpp>
+// #include <opencv2/core/types.hpp>
 
-#include "opencv2/objdetect.hpp"
-#include <opencv2/videoio.hpp> // Video write
+// #include "opencv2/objdetect.hpp"
+// #include <opencv2/videoio.hpp> // Video write
 
 #include <stdlib.h>
 #include <stdio.h>
